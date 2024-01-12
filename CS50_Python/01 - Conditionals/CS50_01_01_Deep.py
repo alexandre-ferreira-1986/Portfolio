ffcd97009dd7346c6539023521743cb0b1eb9db7
Alexandre Ferreira <57299022+alexandre-ferreira-1986@users.noreply.github.com>
2024-01-12 14:21:58 -0300

    Add files via upload

---
+++ CS50_Python/01 - Conditionals/CS50_01_01_Deep.py
@@ -0,0 +1,8 @@
+# Ask the user
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
+
+# If-else to print 'Yes' and 'No'
+if (answer == '42') or (answer == 'forty-two') or (answer == 'forty two'):
+    print('Yes')
+else:
+    print('No')
