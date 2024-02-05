c1c1ced00bf7dacdb2f1315d2567ed87852cc446
Alexandre Ferreira <57299022+alexandre-ferreira-1986@users.noreply.github.com>
2024-02-05 18:49:53 -0300

    Add files via upload

---
+++ CS50_Python/02 - Loops/CS50_03_01_fuel.py
@@ -0,0 +1,34 @@
+# Starting the Loop
+while True:
+
+    # Ask the input
+    fuel = input('Fraction: ')
+
+    try:
+        # Split the input
+        num, den = fuel.split('/')
+
+        # Converting to integers
+        num_int = int(num)
+        den_int = int(den)
+
+        # Calculate
+        frac = num_int/den_int
+
+        # Checking the conditions of the problem
+        if frac <= 1:
+            break
+
+    except (ValueError, ZeroDivisionError):
+        pass
+
+# Transform frac to percentage (mult by 100)
+perc = round(frac * 100)
+
+if perc <= 1:
+    print('E')
+elif perc >= 99:
+    print('F')
+else:
+    print(f'{perc}%')
+
