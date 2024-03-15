212e53a0b5c7d6d00b8ceecda4eefffc3e5e8a20
Alexandre Ferreira <57299022+alexandre-ferreira-1986@users.noreply.github.com>
2024-03-15 11:41:20 -0300

    Create CS50_02_03_twttr
    Solution CS50_02_03_twttr

---
+++ CS50_Python/02 - Loops/CS50_02_03_twttr.py
@@ -0,0 +1,20 @@
+
+def main():
+    text = input('Input: ')
+    
+    vogals = ['a', 'e', 'i', 'o', 'u']
+    new_str = ''
+    
+    for i in text:
+        if i.lower() in vogals:
+            continue
+        else:
+            #new_str += i
+            print(i, end="")
+            
+    #print('Output: ', new_str)
+    
+    
+    
+if __name__ == "__main__":
+    main()
