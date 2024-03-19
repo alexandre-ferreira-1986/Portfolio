359215a9852e311559cd5912076670db682af483
Alexandre Ferreira <57299022+alexandre-ferreira-1986@users.noreply.github.com>
2024-03-19 07:52:15 -0300

    Create CS50_04_04_game.py

---
+++ Python/CS50_Python/04 - Libraries/CS50_04_04_game.py
@@ -0,0 +1,33 @@
+import random
+
+
+
+while True:
+    try: 
+        lev = int(input('Level: '))
+        
+        if lev >= 1:
+            rand_num = random.randint(1, lev)
+            break
+    except:
+        
+        pass
+
+
+while True:
+    try:
+        num_choice = int(input('Guess: '))
+        if num_choice > rand_num:
+            print('Too large!!')
+        elif num_choice < rand_num:
+            print('Too small!!')
+        else:
+            print('Just right!')
+            break
+        
+    
+    except:
+        pass
+        
+    
+
